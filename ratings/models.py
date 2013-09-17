8da6123bb20fa5cd0ec6847e59e15bfc9ff37634
Marek Onuszko <marek.onuszko@gmail.com>
2013-09-17 19:06:58 +0200

    Mostly just testing the git branch workflow

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.contenttypes.models  import ContentType
+from django.contrib.contenttypes import generic
+
+
+class RatedItem(models.Model):
+    score = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+
+class TestDummy(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
