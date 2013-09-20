37249ad8523cf62fabd530516774d27205e4f2f7
Marek Onuszko <marek.onuszko@gmail.com>
2013-09-20 14:08:22 +0200

    models are no longer placeholders, updated README.rst

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models  import ContentType
@@ -10,19 +11,58 @@ class Rating(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
-    score = models.PositiveIntegerField()
+    user = models.ForeignKey(User)
     opinion = models.TextField(blank=True)
 
+    def aggregate_score(self):
+        """The average of user's sub-scores"""
+        # TODO: Cache score to save lookup time
+        scores = Score.objects.filter(rating=self.pk)
+        return sum(Score.normalized() for sc in scores) / len(scores)
 
-class Score(models.Model):
-    """Several scores compose a Rating."""
-    pass
+    class Meta:
+        unique_together = (('user', 'content_type', 'object_id'))
 
 
 class Criteria(models.Model):
     """Scores must evaluate certain criteria. Criteria
     are specific to a given data model."""
-    pass
+    content_type = models.ForeignKey(ContentType)
+    range_min = models.IntegerField()
+    range_max = models.IntegerField()
+
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        if not self.range_min < self.range_max: 
+            raise ValidationError("range_min must be lower than range_max!")
+
+
+class Score(models.Model):
+    """Several scores compose a Rating."""
+    rating = models.ForeignKey(Rating)
+    criteria = models.ForeignKey(Criteria)
+    value = models.IntegerField()
+
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        # I don't put it at the start of the file to play it safe (circular
+        # imports). Squirrels report 'Django is notorious for circular imports'
+        if not self.criteria.range_min <= self.value <= self.criteria.range_max:
+            raise ValidationError("Score value not in Criteria's range!")
+        if self.criteria.content_type != self.rating.content_type:
+            msg = "Rating's ContentType doesn't match Criteria's ContentType!"
+            raise ValidationError(msg)
+
+    def normalized(self, newmin=0, newmax=10):
+        """Return value normalized to another range"""
+        oldmin = self.criteria.range_min
+        oldmax = self.criteria.range_max
+
+        oldspan = oldmax - oldmin
+        newspan = newmax - newmin
+
+        fraction = (self.value - oldmin) / oldspan
+        return newmin + (fraction * newspan)
 
 
 class TestDummy1(models.Model):
