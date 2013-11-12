f5f67adaa464ca8c348ba95f0e6dcde235d27cf2
Marek Onuszko <marek.onuszko@gmail.com>
2013-11-12 16:10:59 +0100

    switched app from inheritance to template tags

--- README.rst
+++ README.rst
@@ -2,8 +2,10 @@
 Ratings
 =======
 
-Ratings is a Django app allowing users to rate arbitrary data models.
+Ratings is a flexible Django app allowing users to rate arbitrary data models.
 Each data model gets its own set of criteria.
+
+Requirements: Python 2.7, Django 1.4.5
 
 Documentation is being added to the ``docs`` directory.
 
@@ -18,50 +20,47 @@ Quick start
     )
 
 2. Include the ratings URLconf in your project urls.py like this::
-    url(r'^ratings/', include('ratings.urls')),
+
+    url(r'^ratings/', include('ratings.urls', namespace='ratings')),
 
 3. Run ``python manage.py syncdb`` to create the ratings models.
 
-4. Modify your ``models.py`` and make your models inherit from RatedModel::
+4. Display scores in your *list* views, like this::
 
-    from ratings.models import RatedModel
-
-
-  class Entry(RatedModel):
-
-
-5. Insert rating forms in your templates, like this::
-
-    {% load url from future %}
     {% load ratings_extras %}
 
+    {% get_list_rating_for entry as lr %}
+    <p>Average: {{ lr.avg_score|floatformat:1 }},
+    voters: {{ lr.num_voters }},
+    standard deviation: {{ lr.std_dev|floatformat:1 }}</p>
 
-    {% if user.is_authenticated %}
-        <form action="{{ entry.get_rate_url }}" method="POST">{% csrf_token %}
-        {% for f in entry|get_forms:user %}
-        {{ f.name }} ({{ f.min }}-{{ f.max }}):
-          {{ f.as_p }}
-        {% endfor %}
-        <input type="submit" value="Submit" />
-    {% endif %}
+5. Display scores in your *detail* views. Start by loading the tags and getting
+   a rating object::
 
-5. Display scores in your templates. In list view::
+    {% load ratings_extras %}
+    
+    {% get_detail_rating_for entry as dr %}
 
-    <p>Average: {{ entry.avg_score|floatformat:1 }}, voters: {{ entry.get_voters }}</p>
+   Display detailed stats::
 
-   In detail view::
-
+    {% get_detail_rating_for entry as dr %}
+    <hr>
     <ul>
-    TOTAL: {{ entry.avg_score }}, Number of voters: {{ custompage.get_voters }}
-    {% for dic in entry.scores_by_crit %}
-    <li>{{ dic.name }}: <b>{{ dic.avg|floatformat:1 }}</b>, deviation: {{ dic.std_dev|floatformat:1 }}, votes: {{ dic.scores }}</li>
-    {% endfor %}
+        TOTAL: {{ dr.avg_score|floatformat:1 }}, Number of voters: {{ dr.num_voters }}
+        {% for dic in dr.scores_by_crit %}
+        <li>{{ dic.name }}: 
+        <b>{{ dic.avg|floatformat:1 }}</b>,
+        deviation: {{ dic.std_dev|floatformat:1 }},
+        votes: {{ dic.scores }}</li>
+        {% endfor %}
     </ul>
+    <hr>
 
-   In detail view, as a list of user comments::
- 
+   Display ratings by individual users, for example::
+
+    <p>Individual opinions:</p>
     <ol>
-    {% for person in entry.scores_by_users %}
+    {% for person in dr.scores_by_users %}
     <li><dl>
         <dt>username</dt>
         <dd>{{ person.username }}</dd>
@@ -83,3 +82,26 @@ Quick start
     {% endfor %}
     </ol>
 
+   Finally, put a rating form in there::
+
+    {% if user.is_authenticated %} 
+    <form action="{{ dr.form_action }}" method="POST">{% csrf_token %}
+            {% for f in dr.get_forms %}
+                {{ f.name }}
+                {{ f.as_p }}
+            {% endfor %}
+        <input type="submit" value="Submit" />
+    {% endif %}
+
+   They won't display properly at first. Read on.
+
+6. Use the admin interface to create ``Criteria`` and ``Choice`` objects.
+   
+   ``Criteria`` are assigned to specific content types, for example Entry
+   or Page. Rating forms are generated based on these criterias.
+
+   ``Choice`` objects determine allowed choices for scores. They are shared
+   among all criterias.
+
+
+Contact: marek.onuszko@gmail.com
--- ratings/templatetags/ratings_extras.py
+++ ratings/templatetags/ratings_extras.py
@@ -1,39 +1,149 @@
+from __future__ import division
+from math import sqrt
+from operator import itemgetter
 from django import template
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models  import ContentType
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
-from ratings.models import Score
+from django.db.models import Avg
+from ratings.models import Score, Criteria
 from ratings.forms import ScoreForm
 
 register = template.Library()
 
+def average(numbers):
+    """Returns average of a sequence of numbers, or 0"""
+    return sum(numbers) / len(numbers) if numbers else 0
 
-@register.assignment_tag(takes_context=True)
-def get_rating_forms(context, ratedobject):
-    user = context['user']
-    ct = ContentType.objects.get_for_model(ratedobject)
-    crits = ct.criteria_set.all()
-    forms = []
-    for crit in crits:
-        forms.append(ScoreForm(prefix=crit.name, user=user, criteria=crit,
-            obj_id=ratedobject.pk))
-    for f in forms:
-        print(f.fields['value'])
-    return forms
+class ListRating:
+    """Provides methods for displaying ratings in list views."""
+    def __init__(self, ratedobject):
+        self.ratedobject = ratedobject
 
-@register.simple_tag
-def rate_url(ratedobject):
-    ct_id = ContentType.objects.get_for_model(ratedobject).pk
-    obj_id = ratedobject.pk
-    return reverse('ratings:rate', kwargs={'ct_id': ct_id, 'obj_id': obj_id})
+    def _get_ct(self):
+        return ContentType.objects.get_for_model(self.ratedobject)
 
-@register.simple_tag
-def voters(ratedobject):
+    def _get_scores(self):
+        ct = self._get_ct()
+        scores = User.objects.filter(
+                score__content_type=ct,
+                score__object_id=self.ratedobject.pk).values(
+                        'username').annotate(average_rating=Avg('score__value'))
+        scores = [row['average_rating'] for row in scores
+                if row['average_rating']]
+        return scores
+
+    def avg_score(self):
+        scores = self._get_scores()
+        return sum(scores) / len(scores) if scores else 0
+
+    def std_dev(self):
+        scores = self._get_scores()
+        avg = self.avg_score()
+        if not avg:
+            return 0
+        else:
+            numerator = sum(pow((s - avg), 2) for s in scores)
+            return sqrt(numerator / len(scores))
+
+    def num_voters(self):
+        ct = self._get_ct()
+        scores = Score.objects.filter(content_type=ct, object_id=self.ratedobject.pk)
+        voters = len({score.user for score in scores})
+        return voters
+
+
+class DetailRating(ListRating):
+    """Provides methods for displaying ratings in detail views."""
+    def __init__(self, ratedobject, user):
+        self.ratedobject = ratedobject
+        self.user = user
+
+    def get_forms(self):
+        #from ratings.forms import ScoreForm # Circular import ;[
+        forms = []
+        ct = self._get_ct()
+        crits = self._get_ct().criteria_set.all()
+        for crit in crits:
+            forms.append(ScoreForm(prefix=crit.name, user=self.user,
+                criteria=crit, obj_id=self.ratedobject.pk))
+        return forms
+
+    def form_action(self):
+        ct_id = self._get_ct().pk
+        obj_id = self.ratedobject.pk
+        return reverse('ratings:rate', kwargs={'ct_id': ct_id, 'obj_id': obj_id})
+
+    def _get_crits(self):
+        ct = self._get_ct()
+        return Criteria.public.filter(content_type=ct)
+
+    def scores_by_crit(self):
+        crits = self._get_crits()
+        result = []
+        for crit in crits:
+            scores = [sc.value for sc in Score.objects.filter(criteria=crit,
+                object_id=self.ratedobject.pk)]
+            avg = sum(scores) / len(scores) if scores else 0
+            # Calculate population standard deviation:
+            if not avg:
+                std_dev = 0
+            else:
+                numerator = sum(pow((s - avg), 2) for s in scores)
+                std_dev = sqrt(numerator / len(scores))
+            result.append({'name': crit.name, 'scores': scores, 'avg': avg,
+                           'std_dev': std_dev})
+        return result
+
+    def scores_by_users(self):
+        result = []
+        scores = Score.objects.filter(object_id=self.ratedobject.pk)
+        users = {}
+        for score in scores:
+            username = score.user.username
+            by_crit = {
+                    'name': score.criteria.name,
+                    'value': score.value,
+                    'comment': score.comment
+                    }
+            if username not in users:
+                users[username] = {
+                        'pub_date': score.pub_date,
+                        'by_crit' : [by_crit]
+                        }
+            else:
+                users[username]['by_crit'].append(by_crit)
+
+        for k, v in users.items():
+            numbers = [crit['value'] for crit in v['by_crit']]
+            avg = average(numbers)
+            by_crit = v['by_crit']
+            by_crit.sort(key=itemgetter('name'))
+            result.append(
+                    {
+                        'username': k,
+                        'pub_date': v['pub_date'],
+                        'by_crit': by_crit,
+                        'avg': avg
+                        }
+                    )
+        result.sort(key=itemgetter('pub_date'), reverse=True)
+        return result
+
+
+@register.assignment_tag
+def get_list_rating_for(ratedobject):
     if isinstance(ratedobject, basestring):
         raise template.TemplateSyntaxError(
-                'The argument should be a model object, not string')
-    ct = ContentType.objects.get_for_model(ratedobject)
-    scores = Score.objects.filter(content_type=ct, object_id=ratedobject.pk)
-    return len({score.user for score in scores})
+                'The argument should be an object, not a string')
+    return ListRating(ratedobject)
+
+
+@register.assignment_tag(takes_context=True)
+def get_detail_rating_for(context, ratedobject):
+    if isinstance(ratedobject, basestring):
+        raise template.TemplateSyntaxError(
+                'The argument should be an object, not a string')
+    return DetailRating(ratedobject, context['user']) # user is for rating form
 
